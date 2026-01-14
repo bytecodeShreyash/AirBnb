7681b20d2d359b5b5bccdd69f11f43b82b1abf9c
Shreyash-Mali <shreyashmali369@gmail.com>
2026-01-14 09:11:22 -0800

    First commit

---
+++ src/main/java/com/example/entity/User.java
@@ -0,0 +1,31 @@
+package com.example.entity;
+
+import com.example.entity.enums.Role;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "app_user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    private String name;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @Enumerated(EnumType.STRING)
+    private Set<Role> roles;
+}
